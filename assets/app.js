505f75c7c7a4ca9f732e814be4bc1f70fc5d1fd2
Josh Lehne <jdlehne@gmail.com>
2017-08-04 11:13:01 -0500

    Worked out some moment

--- assets/app.js
+++ assets/app.js
@@ -1,47 +1,73 @@
+var trainName = "";
+var trainDest = "";
+var trainDepart = "";
+var trainFreq = 0;
+
+
+
+var currentTime = moment().format('h:mm A');
+console.log("The time is now: " + currentTime);
+
 var config = {
     apiKey: "AIzaSyBJjeJOqc11Tyx90rUJ_BYPKngV4yyM5_c",
     authDomain: "jdl-train.firebaseapp.com",
     databaseURL: "https://jdl-train.firebaseio.com",
     projectId: "jdl-train",
-    storageBucket: "",
+    storageBucket: "jdl-train.appspot.com",
     messagingSenderId: "328555815056"
 };
 
 firebase.initializeApp(config);
 
-var database = firebase.database("/trainsAdded");
+var database = firebase.database();
 
-
-var trainName = "";
-var trainDest = "";
-var trainDepart = "";
-var trainFreq = "";
-
-database.ref("/trainsAdded").on("value", function(snapshot) {
-        console.log(snapshot.val());
-        console.log(snapshot.val().trainName);
-        console.log(snapshot.val().trainDest);
-
-
-    },
-    function(errorObject) {
-        console.log("Errors handled: " + errorObject.code);
-    });
-
-("#submitButton").on("click", function(event) {
+$("#submitButton").on("click", function(event) {
     event.preventDefault();
+    console.log("button clicked");
 
     trainName = $("#trainToAdd").val().trim();
-    trainDest = $("#trainDest").val().trim();
-
-    $("#mainTrain").append(trainName);
-
     console.log(trainName);
+    trainDest = $("#trainDest").val().trim();
     console.log(trainDest);
+    trainDepart = $("#trainDepart").val().trim();
+    console.log(trainDepart);
+    trainFreq = $("#trainFreq").val().trim();
+    console.log(trainFreq);
 
-    database.ref("/trainsAdded").set({
+    database.ref().push({
         trainName: trainName,
-        trainDest: trainDest
+        trainDest: trainDest,
+        trainDepart: trainDepart,
+        trainFreq: trainFreq
     });
+
+});
+
+database.ref().on("child_added", function(snapshot) {
+    console.log(snapshot.val().trainName);
+    console.log(snapshot.val().trainDest);
+    console.log(snapshot.val().trainDepart);
+    console.log(snapshot.val().trainFreq);
+
+    var firstTrainMoment = moment(snapshot.val().trainDepart, "hh:mm A").subtract(1, "years");
+    var diffTime = moment().diff(moment(firstTrainMoment), "minutes");
+    var remainder = diffTime % snapshot.val().trainFreq;
+    var minUntilTrain = snapshot.val().trainFreq - remainder;
+    var nextTrain = moment().add(minUntilTrain, "minutes");
+
+    $("#tableBody").append("<tr><th>" + snapshot.val().trainName +
+        " </th><td>" + snapshot.val().trainDest +
+        " </td><td>" + "Every " + snapshot.val().trainFreq + " mins" +
+        " </td><td>" + moment(nextTrain).format("hh:mm A") +
+        " </td><td>" + minUntilTrain + " </td></tr>");
+
+    console.log("Train Name: " + snapshot.val().trainName);
+    console.log("Destination: " + snapshot.val().trainDest);
+    console.log("First Train: " + snapshot.val().trainDepart);
+    console.log("Frequency: " + snapshot.val().trainFreq);
+    console.log("Next Train Time: " + moment(nextTrain).format("hh:mm A"));
+    console.log("Minutes Until: " + minUntilTrain);
+    console.log("====================");
+
 
 });
